348916d08270701ce02b067e5ade47a5452baa4a
Winter Guerra <winterg@mit.edu>
2015-01-31 20:33:26 -0500

    Moving towards using the MRAA library instead of cylon.js

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python 
+
+# This file is the main Karma controller
+
+import mraa
+import time
+
+x = mraa.Pwm(0)
+x.period_us(700)
+x.enable(True)
+value= 0.0
+
+while True:
+    x.write(value)
+    time.sleep(0.05)
+    value = value + 0.01
+    if value >= 1:
+        value = 0.0
